3e0f9f0fd76fa4281bfdcb46e0dfd191ec314c54
muhammadabidyasir139 <muhammadabidyasir139@gmail.com>
2026-01-02 18:35:29 +0700

    Menyiapkan struktur repositori

---
+++ app/src/main/java/com/example/myfirebase/repositori/ContainerApp.kt
@@ -0,0 +1,11 @@
+package com.example.myfirebase.repositori
+
+interface AppContainer {
+    val repositorySiswa: RepositorySiswa
+}
+
+class ContainerApp : AppContainer {
+    override val repositorySiswa: RepositorySiswa by lazy {
+        object : RepositorySiswa {}
+    }
+}
