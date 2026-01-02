d7cfd4bcedf72d3a8b0b42ddca0708750f29d5bd
muhammadabidyasir139 <muhammadabidyasir139@gmail.com>
2026-01-02 19:22:09 +0700

    Update Destinasi Navigasi dan Resource String

--- app/src/main/java/com/example/myfirebase/view/route/DestinasiNavigasi.kt
+++ app/src/main/java/com/example/myfirebase/view/route/DestinasiNavigasi.kt
@@ -18,6 +18,15 @@ object DestinasiEntry : DestinasiNavigasi {
 }
 
 object DestinasiDetail : DestinasiNavigasi {
-    override val route = "item_details"
-    override val titleRes = R.string.app_name
+    override val route = "detail_siswa"
+    override val titleRes = R.string.detail_siswa
+    const val itemIdArg = "idSiswa"
+    val routeWithArgs = "$route/{$itemIdArg}"
+}
+
+object DestinasiEdit : DestinasiNavigasi {
+    override val route = "item_edit"
+    override val titleRes = R.string.edit_siswa
+    const val itemIdArg = "idSiswa"
+    val routeWithArgs = "$route/{$itemIdArg}"
 }
