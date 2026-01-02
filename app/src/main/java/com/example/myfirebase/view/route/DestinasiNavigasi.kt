88fad21a40e80a07f7132d4440b95865e30eb48e
muhammadabidyasir139 <muhammadabidyasir139@gmail.com>
2026-01-02 19:12:50 +0700

    Mengimplementasikan DestinasiNavigasi

--- app/src/main/java/com/example/myfirebase/view/route/DestinasiNavigasi.kt
+++ app/src/main/java/com/example/myfirebase/view/route/DestinasiNavigasi.kt
@@ -1,6 +1,23 @@
 package com.example.myfirebase.view.route
 
+import com.example.myfirebase.R
+
 interface DestinasiNavigasi {
     val route: String
     val titleRes: Int
+}
+
+object DestinasiHome : DestinasiNavigasi {
+    override val route = "home"
+    override val titleRes = R.string.app_name
+}
+
+object DestinasiEntry : DestinasiNavigasi {
+    override val route = "item_entry"
+    override val titleRes = R.string.entry_siswa
+}
+
+object DestinasiDetail : DestinasiNavigasi {
+    override val route = "item_details"
+    override val titleRes = R.string.app_name
 }
