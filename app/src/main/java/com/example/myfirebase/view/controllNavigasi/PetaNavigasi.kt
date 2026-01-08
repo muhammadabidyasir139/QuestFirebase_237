a5f1009e5fcfe975d8e056086e19e0add7cfc8b7
muhammadabidyasir139 <muhammadabidyasir139@gmail.com>
2026-01-08 08:49:09 +0700

    Menambahkan komentar pada PetaNavigasi

--- app/src/main/java/com/example/myfirebase/view/controllNavigasi/PetaNavigasi.kt
+++ app/src/main/java/com/example/myfirebase/view/controllNavigasi/PetaNavigasi.kt
@@ -32,6 +32,7 @@ fun HostNavigasi(
         startDestination = DestinasiHome.route,
         modifier = modifier
     ) {
+        // Halaman Utama
         composable(DestinasiHome.route) {
             HomeScreen(
                 navigateToItemEntry = { navController.navigate(DestinasiEntry.route) },
