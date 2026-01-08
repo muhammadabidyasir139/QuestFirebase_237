8d9f1815df82f7f50da3c7b8d4a41e90a60af2fe
muhammadabidyasir139 <muhammadabidyasir139@gmail.com>
2026-01-08 08:43:32 +0700

    Memperbarui PenyediaViewModel untuk injeksi SavedStateHandle

--- app/src/main/java/com/example/myfirebase/viewmodel/PenyediaViewModel.kt
+++ app/src/main/java/com/example/myfirebase/viewmodel/PenyediaViewModel.kt
@@ -1,19 +1,34 @@
 package com.example.myfirebase.viewmodel
 
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.createSavedStateHandle
 import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
-import com.example.myfirebase.AplikasiDataSiswa
-
-fun CreationExtras.aplikasiDataSiswa(): AplikasiDataSiswa =
-    (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as AplikasiDataSiswa)
+import com.example.myfirebase.SiswaApp
 
 object PenyediaViewModel {
     val Factory = viewModelFactory {
-        initializer { HomeViewModel(aplikasiDataSiswa().container.repositorySiswa) }
-        initializer { EntryViewModel() }
-        initializer { DetailViewModel() }
-        initializer { EditViewModel() }
+        initializer {
+            HomeViewModel(aplikasiSiswa().container.repositorySiswa)
+        }
+        initializer {
+            EntryViewModel(aplikasiSiswa().container.repositorySiswa)
+        }
+        initializer {
+            DetailViewModel(
+                createSavedStateHandle(),
+                aplikasiSiswa().container.repositorySiswa
+            )
+        }
+        initializer {
+            EditViewModel(
+                createSavedStateHandle(),
+                aplikasiSiswa().container.repositorySiswa
+            )
+        }
     }
 }
+
+fun CreationExtras.aplikasiSiswa(): SiswaApp =
+    (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as SiswaApp)
